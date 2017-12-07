31f98fa2ca734ce1c0595352d7f1df99c0b3d07d
Dale Auten <dauten96@gmail.com>
2017-12-07 15:20:32 -0600

    Added shell of the script that will take the alestle.txt files and process them into the finished.csv file

---
+++ Code/processText.py
@@ -0,0 +1,13 @@
+class alestle():
+    def __init__(self, t, u, k, a, p, s):
+        self.title = t
+        self.url = u
+        self.keywords = k
+        self.abstract = a
+        self.pubdate = p
+        self.season = s
+
+    #TODO: return the object with its comma delineated values, including needed default values
+    #for the columns not included here.  A collection alestle.toString()'s should be a valid csv
+    def toString():
+        return ""
