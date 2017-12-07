af4a7cb06d019f56f11b78f8e22aea8bd206e1b7
Dale Auten <dauten96@gmail.com>
2017-12-07 14:27:20 -0600

    Added script that will populate subfolder with the .txt versions of the Alestle documents.  Runs in ~3s (~.1s/pdf) so even full version should create text documents within a few minutes

--- Code/convertToText.py
+++ Code/convertToText.py
@@ -1,7 +1,27 @@
 import subprocess
+import sys
+import os
 
 def getPDFContents(filename):
+    try:
+        subprocess.check_output(["pdftotext", "../Alestle/"+filename, "../Alestle/text/"+filename[:-4]+".txt"])
+    except:
+        pass
 
-    subprocess.check_output(["pdftotext", filename, filename[:-4]+".txt"])
+def getListOfPapers():
+    dir = os.path.dirname(__file__)+"../Alestle"
+    list = []
+    for filename in os.listdir(dir):
+        if(filename.endswith(".pdf")):
+            list.append(filename)
+    if len(list) == 0:
+        print("No files found, quiting")
+        sys.exit()
+    return list
 
-getPDFContents("../Alestle/Ales_1996_01_23.pdf")
+li = getListOfPapers()
+
+print("List obtained: "+li[1])
+
+for l in li:
+    getPDFContents(l)
