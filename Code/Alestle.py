51931673ec0bcea26cab2e5120c6f1fc49562790
Dale Auten <dauten96@gmail.com>
2017-12-08 12:18:30 -0600

    Alestle.toString()

--- Code/Alestle.py
+++ Code/Alestle.py
@@ -1,7 +1,8 @@
+import datetime
+
 class Alestle():
-    def __init__(self, t, u, a, p):
-        #Title := "The Alestle, <Month> <Day>, <Year>"
-        self.title = t
+    def __init__(self, u, a, p):
+        
         self.url = u
         self.author = "Southern Illinois University Edwardsville"
         self.publisher = "Southern Illinois University Edwardsville"
@@ -12,9 +13,14 @@ class Alestle():
 
     #TODO: return the object with its comma delineated values, including needed default values
     #for the columns not included here.  A collection alestle.toString()'s should be a valid csv
-    def toString():
-        return ""
+    def toString(self):
+        d = datetime.datetime.strptime(self.pubdate, "%B%d,%Y").strftime("%B %d, %Y")
+        t = "The Alestle, "+d
+        return t+","+self.url+","+self.url+",,,Southern Illinois University Edwardsville,,,,,,TRUE,,,,,,,FALSE,,,,,,,FALSE,,,,,,,FALSE,,,,,,,,Book,,"+d+","
 
-    def fillClass(inString):
+#    def fillClass(inString):
         #Fils in the variables for the alestle class, given a string containing the entire newspaper.
         
+print(Alestle("google or smth","","January3,1999").toString())
+print(Alestle("google or smth","","February18,2004").toString())
+print(Alestle("google or smth","","April30,2016").toString())
