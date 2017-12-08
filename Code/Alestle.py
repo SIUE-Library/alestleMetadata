6997888fc7439dd1140da347c602f5ba3307c24b
Dale Auten <dauten96@gmail.com>
2017-12-08 14:46:32 -0600

    driver now runs fully

--- Code/Alestle.py
+++ Code/Alestle.py
@@ -11,24 +11,36 @@ class Alestle():
         self.docType = "book"
         self.abstract = "Vol.99,No.99"
         self.pubdate = "January1,1900"
+
         self.fillClass()
 
     #return the object with its comma delineated values, including needed default values
     #for the columns not included here.  A collection alestle.toString()'s should be a valid csv
     def toString(self):
-        d = datetime.datetime.strptime(self.pubdate, "%B%d,%Y").strftime("%B %d, %Y")
-        t = "The Alestle, "+d
-        return t+"|"+self.url+"|"+self.url+"|||Southern Illinois University Edwardsville||||||TRUE|||||||FALSE|||||||FALSE|||||||FALSE||||||||Book||"+d+"|"
+        if self.pubdate == "January1,1900":
+            print("Date is wrong at:")
+        if self.abstract == "Vol.99,No.99":
+            print("Edition is wrong at:")
+        try:
+            d = datetime.datetime.strptime(self.pubdate, "%B%d,%Y").strftime("%B %d, %Y")
+            t = "The Alestle, "+d
+            return t+"|"+self.url+"|"+self.url+"||"+self.abstract+"|Southern Illinois University Edwardsville||||||TRUE|||||||FALSE|||||||FALSE|||||||FALSE||||||||Book||"+d+"|\n"
 
+        except:
+            print("ERROR AT: "+self.url+"\n")
+       
     def fillClass(self):
         #Fils in the variables for the alestle class, given a string containing the entire newspaper.
         testString = open(self.url, "r")
         testString = testString.read()
         testString = testString.replace(" ", "")
+        testString = testString.replace("\n", "")
+        testString = testString.replace("\r", "")
+        
         dateMatch = re.search("(January|February|March|April|May|June|July|August|September|October|November|December)(\d|\d\d,)(\d\d\d\d)",testString)
         if(dateMatch):
             self.pubdate = dateMatch.group(0)
-        issueMatch = re.search("Vol\.(\d|\d\d),No\.(\d\d|\d)",testString)
+        issueMatch = re.search("Vol(,|\.)(\d\d|\d)(,|\.)No(,|\.)(\d\d|\d)",testString)
         if(issueMatch):
             self.abstract = issueMatch.group(0)
         
--- Code/driver.py
+++ Code/driver.py
@@ -1,4 +1,8 @@
 import Alestle
+import os
 
 testObject =  Alestle.Alestle("../Alestle/text/Ales_2002_06_26.txt")
 print(testObject.toString())
+
+for filename in os.listdir("../Alestle/text"):
+    print(Alestle.Alestle("../Alestle/text/"+filename).toString())
