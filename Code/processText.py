9e766b55249e74b036447549e790770982be6c93
Jake <jgrubb@client115-111.wpa.siue.edu>
2017-12-08 11:25:23 -0600

    Added attributes to Alestle class

--- Code/processText.py
+++ Code/processText.py
@@ -1,13 +1,19 @@
 class alestle():
-    def __init__(self, t, u, k, a, p, s):
+    def __init__(self, t, u, a, p):
         self.title = t
         self.url = u
-        self.keywords = k
+        self.author = "Southern Illinois University Edwardsville"
+        self.publisher = "Southern Illinois University Edwardsville"
+        self.city = "Edwardsville"
+        self.docType = "book"
         self.abstract = a
         self.pubdate = p
-        self.season = s
 
     #TODO: return the object with its comma delineated values, including needed default values
     #for the columns not included here.  A collection alestle.toString()'s should be a valid csv
     def toString():
         return ""
+
+    def getInfo(inString):
+        #Fils in the variables for the alestle class, given a string containing the entire newspaper.
+        
