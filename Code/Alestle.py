5a77e92e2ac23c824f29f21ac030f55202d2900b
Dale Auten <dauten96@gmail.com>
2017-12-08 13:00:29 -0600

    changed commas to bars

--- Code/Alestle.py
+++ Code/Alestle.py
@@ -14,21 +14,21 @@ class Alestle():
         self.pubdate = "January1,1900"
         fillClass()
 
-    #TODO: return the object with its comma delineated values, including needed default values
+    #return the object with its comma delineated values, including needed default values
     #for the columns not included here.  A collection alestle.toString()'s should be a valid csv
     def toString(self):
         d = datetime.datetime.strptime(self.pubdate, "%B%d,%Y").strftime("%B %d, %Y")
         t = "The Alestle, "+d
-        return t+","+self.url+","+self.url+",,,Southern Illinois University Edwardsville,,,,,,TRUE,,,,,,,FALSE,,,,,,,FALSE,,,,,,,FALSE,,,,,,,,Book,,"+d+","
+        return t+"|"+self.url+"|"+self.url+"|||Southern Illinois University Edwardsville||||||TRUE|||||||FALSE|||||||FALSE|||||||FALSE||||||||Book||"+d+"|"
 
     def fillClass(self):
         #Fils in the variables for the alestle class, given a string containing the entire newspaper.
-        testString = convertToText.getPDFContents(self.url)
+        testString = open("../Alestle/text/"+self.url).readlines
         testString = testString.replace(" ", "")
         dateMatch = re.search("(January|February|March|April|May|June|July|August|September|October|November|December)(\d|\d\d,)(\d\d\d\d)",testString)
-        if(dateMatch)
+        if(dateMatch):
             self.pubdate = dateMatch.group(0)
         issueMatch = re.search("Vol\.(\d|\d\d),No\.(\d\d|\d)",testString)
-        if(issueMatch)
+        if(issueMatch):
             self.abstract = issueMatch.group(0)
         
