f7fc9f9412508bb706878ef1d260b2c7048a0e76
Jake <jgrubb@client115-111.wpa.siue.edu>
2017-12-08 12:08:41 -0600

    Added regex to find the issue date

--- Code/Alestle.py
+++ Code/Alestle.py
@@ -1,20 +1,31 @@
+import convertToText.py
+import re
 class Alestle():
-    def __init__(self, t, u, a, p):
+    def __init__(fileurl):
         #Title := "The Alestle, <Month> <Day>, <Year>"
-        self.title = t
-        self.url = u
+        self.title = ""
+        self.url = fileURL
         self.author = "Southern Illinois University Edwardsville"
         self.publisher = "Southern Illinois University Edwardsville"
         self.city = "Edwardsville"
         self.docType = "book"
-        self.abstract = a
-        self.pubdate = p
+        self.abstract = ""
+        self.pubdate = "January1,1900"
+        fillClass()
 
     #TODO: return the object with its comma delineated values, including needed default values
     #for the columns not included here.  A collection alestle.toString()'s should be a valid csv
     def toString():
         return ""
 
-    def fillClass(inString):
+    def fillClass():
         #Fils in the variables for the alestle class, given a string containing the entire newspaper.
+        testString = convertToText.getPDFContents(self.url)
+        testString = testString.replace(" ", "")
+        dateMatch = re.search("(January|February|March|April|May|June|July|August|September|October|November|December)(\d|\d\d,)(\d\d\d\d)",testString)
+        if(dateMatch)
+            self.pubdate = dateMatch.group(0)
+        issueMatch = re.search("",testString)
+        if(issueMatch)
+            self.abstract = issueMatch.group(0)
         
